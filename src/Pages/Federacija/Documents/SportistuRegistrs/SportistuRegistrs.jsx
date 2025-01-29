fbf037c6fb383a19651e3ed01b01805ebfffd95d
Morris1337 <117284517+Morris1337@users.noreply.github.com>
2025-01-30 00:38:24 +0200

    nothing

--- src/Pages/Federacija/Documents/SportistuRegistrs/SportistuRegistrs.jsx
+++ src/Pages/Federacija/Documents/SportistuRegistrs/SportistuRegistrs.jsx
@@ -4,10 +4,12 @@ import sportistuRegistrs from './SportistuReģistrs2024.pdf'
 
 export default function SportistuRegistrs() {
   return (
-    <div className="pdf-container">
+    // <div className="pdf-container">
+    <div style={{ width: '100%', height: '100vh', overflow: 'auto' }}>
         <iframe 
         src={sportistuRegistrs} 
-        className="pdf-viewer"
+        // className="pdf-viewer"
+        style={{ width: '100%', height: '100%', border: 'none' }}
         title="Sportistu Registrs PDF"
       />
     </div>
